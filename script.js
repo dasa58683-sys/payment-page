861163ca8e2e6486accf14f20f23a69aa55e372f
dasa58683-sys <dasa58683@gmail.com>
2026-07-20 17:29:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const btn = document.querySelector(".view-details");
+const dropdown = document.querySelector(".amount-dropdown");
+const icon = btn.querySelector("i");
+
+btn.addEventListener("click", () => {
+
+    if(dropdown.style.display === "block"){
+
+        dropdown.style.display = "none";
+        icon.classList.replace("fa-angle-up","fa-angle-down");
+
+    }else{
+
+        dropdown.style.display = "block";
+        icon.classList.replace("fa-angle-down","fa-angle-up");
+
+    }
+
+});
+
+// PAYMENT METHOD CARD
+
+const tops = document.querySelectorAll(".payment-top");
+
+tops.forEach((top) => {
+
+    top.addEventListener("click", () => {
+
+        const card = top.parentElement;
+
+        const allCards =
+        document.querySelectorAll(".payment-method-card");
+
+
+        allCards.forEach((item) => {
+
+            if(item !== card){
+
+                item.classList.remove("active");
+
+            }
+
+        });
+
+        card.classList.toggle("active");
+
+    });
+
+});
